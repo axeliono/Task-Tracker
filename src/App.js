6bbf482f932e026d10e1257d5cc690bc064b00d3
axeliono <69375446+axeliono@users.noreply.github.com>
2021-01-23 18:22:44 -0600

    getting task components and functions for adding, removing task and changing reminder state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "See doctor",
+      day: "Jan 21st at 1 pm",
+      reminder: "true",
+    },
+    {
+      id: 2,
+      text: "check on parents",
+      day: "Jan 22nd at 10am",
+      reminder: "true",
+    },
+    {
+      id: 3,
+      text: "ask friend how they're doing",
+      day: "Jan 23rd at 12pm",
+      reminder: "false",
+    },
+  ]);
+
+  // DELETE TASK
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  //TOGGLE REMINDER
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
-      <h1>Hello From React</h1>
       <Header />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Current Tasks"
+      )}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,25 @@
-const Header = () => {
+import PropTypes from "prop-types";
+import Button from "./Button";
+
+const Header = ({ title }) => {
+  const onClick = () => {
+    console.log("Click");
+  };
+
   return (
-    <header>
-      <h1>Hello From React</h1>
+    <header className="header">
+      <h1>{title}</h1>
+      <Button onClick={onClick} />
     </header>
   );
+};
+
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
 };
 
 export default Header;
